e39e388c7553e78557bc2ee24aa28ca4a7f813f8
Jessie Huff <huffjd1@gmail.com>
2018-03-09 17:38:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(range){
   return function(start, end){
     let distance = Math.abs(parseInt(end) - parseInt(start))
-    let difference = blockRange
+    let difference = range
   }
 }
